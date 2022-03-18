65e501490f27f6001796f7180ab8fb4b5bd2d1ca
ali ali <ali.ali.deve@gmail.com>
2022-03-19 01:58:34 +0200

    fix

--- Repositories/InMemItemsRepository.cs
+++ Repositories/InMemItemsRepository.cs
@@ -1,4 +1,7 @@
 using Catalog.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 namespace Catalog.Repositories
 {
     class InMemItesmRepository
@@ -9,5 +12,13 @@ namespace Catalog.Repositories
             new Item { id = Guid.NewGuid(), Name = "a2", Price = 200, CreatedDate = DateTimeOffset.UtcNow },
             new Item { id = Guid.NewGuid(), Name = "a3", Price = 300, CreatedDate = DateTimeOffset.UtcNow },
         };
+
+        public IEnumerable<Item> getItems(){
+            return items;
+        }
+
+        public Item getItem(Guid id){
+            return items.Where( item => item.id == id).SingleOrDefault();
+        }
     }
 }
